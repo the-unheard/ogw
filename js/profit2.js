fe30a8015dcd282398ead042438a128d64168a46
Jeff <unvedicus@gmail.com>
2022-12-27 18:58:39 +0800

    feat: added a better profit section
    Signed-off-by: Jeff <unvedicus@gmail.com>

--- js/profit2.js
+++ js/profit2.js
@@ -36,7 +36,7 @@ class Main {
 		var profit, width, highestPlayerProfit;
 
 		$('.profit-playerProfits').each((i) =>{
-			profit = parseFloat($('.profit-playerProfits').eq(1).text());
+			profit = parseFloat($('.profit-playerProfits').eq(i).text());
 			profits.push(profit);
 		});
 
