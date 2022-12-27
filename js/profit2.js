24027b9d0936544adea091de542b71fe454ec42c
Jeff <unvedicus@gmail.com>
2022-12-27 19:44:46 +0800

    feat: added a better profit section
    Signed-off-by: Jeff <unvedicus@gmail.com>

--- js/profit2.js
+++ js/profit2.js
@@ -27,7 +27,7 @@ class Main {
 
 	}
 
-	tooltTipEnabler() {
+	toolTipEnabler() {
 		var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 		var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 			return new bootstrap.Tooltip(tooltipTriggerEl)
