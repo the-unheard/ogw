bffa112f1d0f66d28a46377798c28e8e60c3a819
Jeff <unvedicus@gmail.com>
2023-01-01 14:30:18 +0800

    Changelog - restyled the format
    Signed-off-by: Jeff <unvedicus@gmail.com>

--- php/changelog.php
+++ php/changelog.php
@@ -1,63 +1,63 @@
 <h6>Dec 2022</h6>
-  <ul>
-    <li>31: Added Meed and Cafe in Profits</li>
-    <li>31: Added table sorting in Profits</li>
-    <li>30: Reworked Profits</li>
-    <li>30: Updated Plenny's in Profits</li>
-    <li>30: Added Player Shop in Profits</li>
-    <li>20: Added Ooblet accessories in Clothings</li>
-    <li>19: Added Player clothings in Clothings</li>
-    <li>19: Updated names and prices in Furnitures</li>
-    <li>19: Added all new furnitures in Furnitures</li>
+  <ul class="list-unstyled">
+    <li><span class="changelog-date">31</span> Added Meed and Cafe in Profits</li>
+    <li><span class="changelog-date">31</span> Added table sorting in Profits</li>
+    <li><span class="changelog-date">30</span> Reworked Profits</li>
+    <li><span class="changelog-date">30</span> Updated Plenny's in Profits</li>
+    <li><span class="changelog-date">30</span> Added Player Shop in Profits</li>
+    <li><span class="changelog-date">19</span> Added Ooblet accessories in Clothings</li>
+    <li><span class="changelog-date">19</span> Added Player clothings in Clothings</li>
+    <li><span class="changelog-date">19</span> Updated names and prices in Furnitures</li>
+    <li><span class="changelog-date">19</span> Added all new furnitures in Furnitures</li>
   </ul>
 <h6>Nov 2022</h6>
-  <ul>
-    <li>13: Added filter feature for map</li>
-    <li>13: Updated map locations</li>
+  <ul class="list-unstyled">
+    <li><span class="changelog-date">13</span> Added filter feature for map</li>
+    <li><span class="changelog-date">13</span> Updated map locations</li>
   </ul>
 <h6>Sep 2022</h6>
-  <ul>
-    <li>05: Updated all cards in Almanac</li>
-    <li>05: Added new ooblets, bios, regions in Almanac</li>
+  <ul class="list-unstyled">
+    <li><span class="changelog-date">05</span> Updated all cards in Almanac</li>
+    <li><span class="changelog-date">05</span> Added new ooblets, bios, regions in Almanac</li>
   </ul>
 <h6>May 2022</h6>
-  <ul>
-    <li>23: Complete website overhaul</li>
-    <li>23: Implemented responsive design</li>
-    <li>23: Added a spoiler-free home page</li>
-    <li>23: Reworked the filter in Almanac</li>
-    <li>23: Performance improvements in Farm planner</li>
-    <li>23: Angkze and Quabbo pixel reworked</li>
-    <li>23: Added Bibbin in Farm Planner</li>
-    <li>23: Performance improvements in Playtest</li>
-    <li>23: More samples in Farm Planner</li>
+  <ul class="list-unstyled">
+    <li><span class="changelog-date">23</span> Complete website overhaul</li>
+    <li><span class="changelog-date">23</span> Implemented responsive design</li>
+    <li><span class="changelog-date">23</span> Added a spoiler-free home page</li>
+    <li><span class="changelog-date">23</span> Reworked the filter in Almanac</li>
+    <li><span class="changelog-date">23</span> Performance improvements in Farm planner</li>
+    <li><span class="changelog-date">23</span> Angkze and Quabbo pixel reworked</li>
+    <li><span class="changelog-date">23</span> Added Bibbin in Farm Planner</li>
+    <li><span class="changelog-date">23</span> Performance improvements in Playtest</li>
+    <li><span class="changelog-date">23</span> More samples in Farm Planner</li>
   </ul>
 <h6>Dec 2021</h6>
-  <ul>
-    <li>29: Added Bibbin in Almanac</li>
-    <li>29: Updated Jama and Pantsabear in Almanac</li>
-    <li>29: Updated catch rates in Fishing</li>
-    <li>29: Added new furnitures</li>
-    <li>29: Added new recipes</li>
-    <li>29: Added new wishy well items</li>
-    <li>29: Updated slurry items</li>
+  <ul class="list-unstyled">
+    <li><span class="changelog-date">29</span> Added Bibbin in Almanac</li>
+    <li><span class="changelog-date">29</span> Updated Jama and Pantsabear in Almanac</li>
+    <li><span class="changelog-date">29</span> Updated catch rates in Fishing</li>
+    <li><span class="changelog-date">29</span> Added new furnitures</li>
+    <li><span class="changelog-date">29</span> Added new recipes</li>
+    <li><span class="changelog-date">29</span> Added new wishy well items</li>
+    <li><span class="changelog-date">29</span> Updated slurry items</li>
   </ul>
 <h6>Jun 2021</h6>
-  <ul>
-    <li>06: Updated Farm Planner</li>
-    <li>06: Added new almanac entries</li>
-    <li>06: Updated fishing rates</li>
-    <li>06: Added fishing rate calculator</li>
-    <li>06: Added new furnitures</li>
-    <li>06: Added new profitable items</li>
-    <li>06: Added new unlockables</li>
+  <ul class="list-unstyled">
+    <li><span class="changelog-date">06</span> Updated Farm Planner</li>
+    <li><span class="changelog-date">06</span> Added new almanac entries</li>
+    <li><span class="changelog-date">06</span> Updated fishing rates</li>
+    <li><span class="changelog-date">06</span> Added fishing rate calculator</li>
+    <li><span class="changelog-date">06</span> Added new furnitures</li>
+    <li><span class="changelog-date">06</span> Added new profitable items</li>
+    <li><span class="changelog-date">06</span> Added new unlockables</li>
   </ul>
 <h6>Feb 2021</h6>
-  <ul>
-    <li>06: Added Oobcoop Lv5 to the Farm Planner</li>
-    <li>06: Added Farm Planner image export feature</li>
-    <li>06: Added true fishing rates</li>
-    <li>06: Added Wildland's upgrade list</li>
-    <li>06: Added furnitures, paint, and more</li>
+  <ul class="list-unstyled">
+    <li><span class="changelog-date">06</span> Added Oobcoop Lv5 to the Farm Planner</li>
+    <li><span class="changelog-date">06</span> Added Farm Planner image export feature</li>
+    <li><span class="changelog-date">06</span> Added true fishing rates</li>
+    <li><span class="changelog-date">06</span> Added Wildland's upgrade list</li>
+    <li><span class="changelog-date">06</span> Added furnitures, paint, and more</li>
   </ul>
 
