5373d5f589355f7360764bfb2d1b815e53ec4525
Jeff <unvedicus@gmail.com>
2023-11-09 14:44:09 +0800

    Changed glimmyglobe images to clickable img from div

--- js/import-glimmyglobes.js
+++ js/import-glimmyglobes.js
@@ -6,28 +6,36 @@ const glimmyglobesData = ['angkze', 'bibbin', 'bittle', 'bristlebud', 'bunglebee
 
 const globeType = ['basic', 'exquisite', 'fancy'];
 const oobletRarity = ['common', 'uncommon', 'gleamy'];
+let fullName;
 
 toTitleCase = (str) => {return str.replace(/(?:^|\s)\w/g, (match) => { return match.toUpperCase()})}
 
 glimmyglobesData.forEach(ooblet =>{
     oobletRarity.forEach(rarity => {
         globeType.forEach(type => {
+            fullName = toTitleCase(rarity) + ' ' + toTitleCase(ooblet) + ' - ' + toTitleCase(type);
+
             $('.glimmyglobes-' + rarity).append(
                 $(document.createElement('div'))
                     .addClass('glimmyglobes-item d-inline-block m-2 p-3 align-top position-relative')
                     .append(
-                        $(document.createElement('div'))
-                            .addClass('glimmyglobes-img mx-auto')
-                            .css('background-image', 'url("images/glimmyglobes/small/'+ rarity[0] +'_' + ooblet + '_'+ type[0] +'.jpg")')
+                        $(document.createElement('a'))
+                            .addClass('glimmyglobes-link')
+                            .attr('href', 'images/glimmyglobes/small/'+ rarity[0] +'_' + ooblet + '_'+ type[0] +'.jpg')
+                            .append(
+                                $(document.createElement('img'))
+                                    .addClass('glimmyglobes-img mx-auto')
+                                    .attr('src', 'images/glimmyglobes/small/'+ rarity[0] +'_' + ooblet + '_'+ type[0] +'.jpg"')
+                                    .attr('alt', fullName)
+                            )
                     )
                     .append(
                         $(document.createElement('p'))
                             .addClass('glimmyglobes-name og-ellipsis')
-                            .attr('title', toTitleCase(rarity) + ' ' + toTitleCase(ooblet) + ' - ' + toTitleCase(type) + ' Glimmyglobe')
-                            .text(rarity + ' ' + ooblet + ' - ' + type)
+                            .attr('title', fullName)
+                            .text(fullName)
                     )
             )
         })
     })
-
 })
